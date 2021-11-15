459b5699e47a7262290ee3dc97acba98e498013b
Farhana Uddin <far@wireless-10-105-123-120.umd.edu>
2021-11-14 19:47:11 -0500

    zodiac method

--- ZodiacSign.py
+++ ZodiacSign.py
@@ -1,66 +1,66 @@
 import random
 
-def zodiac_sign(month=str(input("Enter birth month: ")),  day=int(input("Enter birth day: "))):
-    if month.lower() == "January":
-        if day <= 19:
-            result = "Capricorn"
-        else:
-            result = "Aquarius"
-    elif month.lower() == "February":
-        if day <= 18:
-            result = "Aquarius"
-        else:
-            result = "Pisces"
-    elif month.lower() == "March":
-        if day <= 20:
-            result = "Pisces"
-        else:
-            result = "Aries"
-    elif month.lower() == "April":
-        if day <= 19:
-            result = "Aries"
-        else:
-            result = "Taurus"
-    elif month.lower == "May":
-        if day <= 20:
-            result = "Taurus"
-        else:
-            result = "Gemini"
-    elif month.lower() == "June":
-        if day <= 20:
-            result = "Gemini"
-        else:
-            result = "Cancer"
-    elif month.lower == "July":
-        if day <= 22:
-            result = "Cancer"
-        else:
-            result = "Leo"
-    elif month.lower == "August":
-        if day <= 22:
-            result = "Leo"
-        else:
-            result = "Virgo"
-    elif month.lower == "September":
-        if day <= 22:
-            result = "Virgo"
-        else:
-            result = "Libra"
-    elif month.lower() == "October":
-        if day <= 22:
-            result = "Libra"
-        else:
-            result = "Scorpio"
-    elif month.lower == "November":
-        if day <= 21:
-            result = "Scorpio"
-        else:
-            result = "Sagittarius"
-    elif month.lower == "December":
-        if day <= 21:
-            result = "Sagittarius"
-        else:
-            result = "Capricorn"
+def zodiac_sign(month, day):
+  if month == "January":
+    if day <= 19:
+      return "Capricorn"
+    else:
+      return "Aquarius"
+  elif month == "February":
+    if day <= 18:
+      return "Aquarius"
+    else:
+      return "Pisces"
+  elif month == "March":
+    if day <= 20:
+      return "Pisces"
+    else:
+      return "Aries"
+  elif month == "April":
+    if day <= 19:
+      return "Aries"
+    else:
+      return "Taurus"
+  elif month == "May":
+    if day <= 20:
+      return "Taurus"
+    else:
+      return "Gemini"
+  elif month == "June":
+    if day <= 20:
+      return "Gemini"
+    else:
+      return "Cancer"
+  elif month == "July":
+    if day <= 22:
+      return "Cancer"
+    else:
+      return "Leo"
+  elif month == "August":
+    if day <= 22:
+      return "Leo"
+    else:
+      return "Virgo"
+  elif month == "September":
+    if day <= 22:
+      return "Virgo"
+    else:
+      return "Libra"
+  elif month == "October":
+    if day <= 22:
+      return "Libra"
+    else:
+      return "Scorpio"
+  elif month == "November":
+    if day <= 21:
+      return "Scorpio"
+    else:
+      return "Sagittarius"
+  elif month == "December":
+    if day <= 21:
+      return "Sagittarius"
+    else:
+      return "Capricorn"
 
 
 
